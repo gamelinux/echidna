d3dec7db971087f00e4e92da963fda02402482f2
firnsy <firnsy@securixlive.com>
2011-06-09 21:24:24 -0700

    fixed: removed explict engine type and validated sql.

--- ansmf/sql/nsmf.sql
+++ ansmf/sql/nsmf.sql
@@ -1,20 +1,22 @@
-CREATE  TABLE IF NOT EXISTS `nsmf`.`nsmf_agent` (
-  `agent_id` INT NOT NULL AUTO_INCREMENT ,
+CREATE TABLE IF NOT EXISTS `nsmf`.`nsmf_agent` (
+  `agent_id` INT NOT NULL AUTO_INCREMENT,
   `agent_name` VARCHAR(45) NOT NULL ,
   `agent_password` VARCHAR(45) NOT NULL ,
   `agent_description` VARCHAR(45) NULL ,
   `agent_ip` INT NULL ,
   `agent_network` VARCHAR(45) NULL ,
   `agent_active` TINYINT(1)  NOT NULL DEFAULT 0 ,
-  PRIMARY KEY (`agent_id`) ,
-  UNIQUE INDEX `agent_name_UNIQUE` (`agent_name` ASC) )
-ENGINE = MyISAM
+  PRIMARY KEY (`agent_id`)
+);
 
-CREATE  TABLE IF NOT EXISTS `nsmf`.`nsmf_node` (
-  `node_id` INT NOT NULL AUTOINCREMENT,
+CREATE UNIQUE INDEX `agent_name_UNIQUE` ON `nsmf_agent` (`agent_name` ASC);
+
+CREATE TABLE IF NOT EXISTS `nsmf`.`nsmf_node` (
+  `node_id` INT NOT NULL AUTO_INCREMENT,
   `node_name` VARCHAR(45) NOT NULL ,
   `node_description` VARCHAR(45) NULL ,
   `node_type` VARCHAR(45) NOT NULL ,
-  PRIMARY KEY (`node_id`) )
-  UNIQUE INDEX `node_name_UNIQUE` (`node_name` ASC) )
-ENGINE = MyISAM
+  PRIMARY KEY (`node_id`)
+);
+
+CREATE  UNIQUE INDEX `node_name_UNIQUE` ON `nsmf_node` (`node_name` ASC);
