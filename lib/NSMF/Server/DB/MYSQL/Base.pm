44a331b3101e9a855381ee393ca2bf73d06be3b0
firnsy <firnsy@securixlive.com>
2011-10-18 13:04:51 +1100

    updated: enhanced filter creation by modelling on mongodb's nomenclature.

--- lib/NSMF/Server/DB/MYSQL/Base.pm
+++ lib/NSMF/Server/DB/MYSQL/Base.pm
@@ -26,6 +26,8 @@ use warnings;
 use strict;
 use v5.10;
 
+use Data::Dumper;
+
 #
 # NSMF INCLUDES
 #
@@ -156,37 +158,122 @@ sub create_filter
 {
     my ($self, $filter) = @_;
 
-    my @fields = keys( %{ $filter } );
+    if( ref($filter) ne 'HASH' ) {
+        return '';
+    }
+
+    return 'WHERE ' . $self->create_filter_from_hash($filter);
+}
+
+
+sub create_filter_from_hash {
+    my ($self, $value, $field, $parent_field) = @_;
+
+    if ( defined( $field ) ) {
+        $value = $value->{$field};
+    }
+
+    my @fields  = keys( %{ $value } );
 
     return '' if ( @fields == 0 );
 
     my @where = ();
     my $connect = 'AND';
+    my $conditional = '=';
 
     # build up the search criteria
-    for my $field ( @fields )
-    {
+    for my $f ( @fields ) {
         my $criteria = '';
 
-        if ( ref($filter->{$field}) eq 'ARRAY' )
+        given( $f )
         {
-
-
-        }
-        # 
-        elsif ( $filter->{$field} =~ m/[^\d]/ )
-        {
-            $criteria = $field . "=" . $self->{__handle}->quote($filter->{$field});
-        }
-        else
-        {
-            $criteria = $field . '=' . $filter->{$field};
+            when(/\$eq/) { $conditional = '='; }
+            when(/\$ne/) { $conditional = '!='; }
+            when(/\$lt/) { $conditional = '<'; }
+            when(/\$lte/) { $conditional = '<='; }
+            when(/\$gt/) { $conditional = '>'; }
+            when(/\$gte/) { $conditional = '>='; }
         }
 
-        push(@where, $criteria);
+        if ( ref($value->{$f}) eq 'ARRAY' )
+        {
+            my $c = $self->create_filter_from_array($value, $f, $field);
+            push( @where, $c ) if ( length($c) );
+        }
+        elsif ( ref($value->{$f}) eq 'HASH' )
+        {
+            my $c = $self->create_filter_from_hash($value, $f, $field);
+            push( @where, $c ) if ( length($c) );
+        }
+        else {
+            my $c = $self->create_filter_from_scalar($value->{$f}, $f, $field, $conditional);
+            push( @where, $c ) if ( length($c) );
+        }
     }
 
-    return 'WHERE ' . join(" $connect ", @where);
+    return '(' . join(" $connect ", @where) . ')';
+}
+
+
+sub create_filter_from_array {
+    my ($self, $value, $field, $parent_field) = @_;
+
+    if ( defined( $field ) ) {
+        $value = $value->{$field};
+    }
+
+    my @fields = @{ $value };
+
+    return '' if ( @fields == 0 );
+
+    my @where = ();
+    my $connect = '';
+
+    given( $field )
+    {
+        when(/\$nor/) { $connect = 'NOT OR'; $field = undef; }
+        when(/\$or/) { $connect = 'OR'; $field = undef; }
+        when(/\$and/) { $connect = 'AND'; $field = undef; }
+        when(/\$in/) {
+            return '(' . $parent_field . ' IN (' . join(",", @{ $value }) . '))';
+        }
+        when(/\$nin/) {
+            return '(' . $parent_field . ' NOT IN (' . join(",", @{ $value }) . '))';
+        }
+    }
+
+    # build up the search criteria
+    for my $f ( @fields ) {
+        my $criteria = '';
+
+        if ( ref($f) eq 'ARRAY' )
+        {
+            my $c = $self->create_filter_from_array($f, $field);
+            push( @where, $c ) if ( length($c) );
+        }
+        elsif ( ref($f) eq 'HASH' )
+        {
+            my $c = $self->create_filter_from_hash($f, $field);
+            push( @where, $c ) if ( length($c) );
+        }
+    }
+
+    return '(' . join(" $connect ", @where) . ')';
+}
+
+
+sub create_filter_from_scalar {
+    my ($self, $value, $field, $parent_field, $conditional) = @_;
+
+    $conditional //= '=';
+    $field = $parent_field if ( $field =~ /^\$/ );
+
+    if ( $value =~ m/[^\d]/ )
+    {
+        return $field . $conditional . $self->{__handle}->quote($value);
+    }
+
+    return $field . $conditional . $value;
 }
 
 1;
