67f6c8df0571b03f9eb576a84905a01627407915
firnsy <firnsy@securixlive.com>
2011-10-19 09:59:22 +1100

    added: defined more client api stubs for the core module and implemented server uptime and version queries.

--- lib/NSMF/Server/Component/CORE.pm
+++ lib/NSMF/Server/Component/CORE.pm
@@ -44,7 +44,7 @@ use NSMF::Common::Logger;
 #
 my $logger = NSMF::Common::Logger->new();
 
-my $nsmf    = NSMF::Server->new();
+my $nsmf    = NSMF::Server->instance();
 my $config  = $nsmf->config;
 my $modules =["core", @{ $config->modules() }];
 
@@ -64,50 +64,98 @@ sub init {
           "exec" => \&get_modules_available,
           "acl" => 0,
         },
+
+        "register_agent" => {
+          "help" => "Register an agent to the Echidna framework.",
+          "exec" => \&agent_register,
+          "acl" => 128,
+        },
+        "unregister_agent" => {
+          "help" => "Unregister an agent, and all subordinate nodes, from the Echidna framework.",
+          "exec" => \&agent_unregister,
+          "acl" => 128,
+        },
         "register_node" => {
-          "help" => "Register a node to the NSMF framework.",
-          "exec" => sub{},
+          "help" => "Register a node to the Echidna framework.",
+          "exec" => \&node_register,
           "acl" => 128,
         },
         "unregister_node" => {
-          "help" => "Unregister a node from the NSMF framework.",
-          "exec" => sub{},
+          "help" => "Unregister a node from the Echidna framework.",
+          "exec" => \&node_unregister,
           "acl" => 128,
         },
         "register_client" => {
-          "help" => "Register a client to the NSMF framework.",
-          "exec" => sub{ },
+          "help" => "Register a client to the Echidna framework.",
+          "exec" => \&client_register,
           "acl" => 127,
         },
         "unregister_client" => {
-          "help" => "Unregister a client from the NSMF framework.",
+          "help" => "Unregister a client from the Echidna framework.",
+          "exec" => \&client_unregister,
+          "acl" => 127,
+        },
+
+        "subscribe_agent" => {
+          "help" => "Subscribe to an agent's, and all subordinate nodes', broadcasts.",
           "exec" => sub{ },
           "acl" => 127,
         },
-        "subscribe_netgroup" => {
-          "help" => "Subscribe to specified netgroup on the NSMF framework.",
+        "unsubscribe_agent" => {
+          "help" => "Unsubscribe from an agent's, and all subordinate nodes', broadcasts.",
           "exec" => sub{ },
           "acl" => 127,
         },
-        "unsubscribe_netgroup" => {
-          "help" => "Unsubscribe from a specified netgroup on the NSMF framework.",
+        "subscribe_node" => {
+          "help" => "Subscribe to a node's broadcasts.",
+          "exec" => sub{ },
+          "acl" => 127,
+        },
+        "unsubscribe_node" => {
+          "help" => "Unsubscribe from a node's broadcasts.",
           "exec" => sub{ },
           "acl" => 127,
         },
 
+        "server_version" => {
+          "help" => "Returns the Version and revision of the Echidna server.",
+          "exec" => \&get_server_version,
+          "acl" => 127,
+        },
+        "server_uptime" => {
+          "help" => "Returns the current uptime, in seconds, of the Echidna server.",
+          "exec" => \&get_server_uptime,
+          "acl" => 127,
+        },
+
+        "search_agent" => {
+            "help" => "Search for agents.",
+            "exec" => \&get_event_details,
+            "acl" => 255,
+        },
+        "search_node" => {
+            "help" => "Search for nodes.",
+            "exec" => \&get_event_details,
+            "acl" => 255,
+        },
+        "search_client" => {
+            "help" => "Search for clients.",
+            "exec" => \&get_event_details,
+            "acl" => 255,
+        },
         "search_event" => {
             "help" => "Search for events.",
-            "exec" => \&search_event,
+            "exec" => \&get_event_details,
             "acl" => 0,
         },
         "search_session" => {
             "help" => "Search for sessions.",
-            "exec" => \&search_session,
+            "exec" => \&get_session_details,
             "acl" => 0,
         },
         "search_data" => {
             "help" => "Search for full-content data.",
-            "exec" => \&search_data,
+            "exec" => \&get_data_details,
             "acl" => 0,
         },
     });
@@ -144,13 +192,199 @@ sub get_modules_available
 }
 
 
+#
+# CORE REGISTRATION
+#
+
+sub agent_register {
+    my ($self, $params) = @_;
+
+}
+
+sub agent_unregister {
+    my ($self, $params) = @_;
+
+}
+
+sub node_register {
+    my ($self, $params) = @_;
+
+}
+
+sub node_unregister {
+    my ($self, $params) = @_;
+
+}
+
+sub client_register {
+    my ($self, $params) = @_;
+
+}
+
+sub client_unregister {
+    my ($self, $params) = @_;
+
+}
+
+
+#
+# CORE SUBSCRIPTION
+#
+
+sub agent_subscribe {
+    my ($self, $params) = @_;
+
+}
+
+sub agent_unsubscribe {
+    my ($self, $params) = @_;
+
+}
+
+sub node_subscribe {
+    my ($self, $params) = @_;
+
+}
+
+sub node_unsubscribe {
+    my ($self, $params) = @_;
+
+}
+
+
+#
+# CORE GENERAL REQUESTS
+#
+
+sub get_server_version {
+    my ($self, $params) = @_;
+
+    my $version = $nsmf->{__version};
+
+    return $version;
+}
+
+sub get_server_uptime {
+    my ($self, $params) = @_;
+
+    my $uptime = time() - $nsmf->{__started};
+
+    return $uptime;;
+}
+
+sub get_server_queue {
+    my ($self, $params) = @_;
+
+}
+
+sub get_server_status {
+    my ($self, $params) = @_;
+
+}
+
+
+sub get_node_version {
+    my ($self, $params) = @_;
+    return 'ALPHA';
+}
+
+sub get_node_uptime {
+    my ($self, $params) = @_;
+
+}
+
+sub get_node_queue {
+    my ($self, $params) = @_;
+
+}
+
+sub get_node_status {
+    my ($self, $params) = @_;
+
+}
+
+
+sub get_plugins_loaded {
+    my ($self, $params) = @_;
+
+}
+
+sub get_plugin_info {
+    my ($self, $params) = @_;
+
+}
+
+
+sub get_clients_connected {
+    my ($self, $params) = @_;
+
+}
+
+sub get_client_info {
+    my ($self, $params) = @_;
+
+}
+
+
+sub get_nodes_connected {
+    my ($self, $params) = @_;
+
+}
+
+sub get_node_info {
+    my ($self, $params) = @_;
+
+}
+
 
 
 #
 # CORE STRUCTURE SEARCHES
 #
 
-sub search_event {
+sub get_agent_details {
+    my ($self, $params) = @_;
+
+    my $db = NSMF::Server->database();
+
+    # TODO: validate params
+
+    my $ret = $db->search({
+        agent => $params
+    });
+
+    return $ret;
+}
+
+sub get_node_details {
+    my ($self, $params) = @_;
+
+    my $db = NSMF::Server->database();
+
+    # TODO: validate params
+
+    my $ret = $db->search({
+        agent => $params
+    });
+
+    return $ret;
+}
+
+sub get_client_details {
+    my ($self, $params) = @_;
+
+    my $db = NSMF::Server->database();
+
+    # TODO: validate params
+
+    my $ret = $db->search({
+        agent => $params
+    });
+
+    return $ret;
+}
+
+sub get_event_details {
     my ($self, $params) = @_;
 
     my $db = NSMF::Server->database();
@@ -164,7 +398,7 @@ sub search_event {
     return $ret;
 }
 
-sub search_session {
+sub get_session_details {
     my ($self, $params) = @_;
 
     my $db = NSMF::Server->database();
@@ -178,7 +412,7 @@ sub search_session {
     return $ret;
 }
 
-sub search_data {
+sub get_data_details {
     my ($self, $params) = @_;
 
     my $db = NSMF::Server->database();
